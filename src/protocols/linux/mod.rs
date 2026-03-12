24ce99159472c010e62ea7b95413a637f2be85c8
AlexVplle <alexisvapaille11@gmail.com>
2026-03-12 22:42:04 +0100

    refactor: move Linux boot protocol preparation into bootloader-core

--- src/protocols/linux/mod.rs
+++ src/protocols/linux/mod.rs
@@ -3,7 +3,10 @@ pub mod constants;
 use core::ptr;
 
 use constants::*;
-use super::boot_protocol::BootProtocol;
+use crate::arch::Architecture;
+use crate::firmware::FirmwareInterface;
+use crate::framebuffer::FramebufferInfo;
+use crate::protocols::boot_protocol::BootProtocol;
 
 pub struct Linux;
 
@@ -13,7 +16,8 @@ impl BootProtocol for Linux {
             if kernel_size < HEADER_MIN_SIZE {
                 return false;
             }
-            let boot_flag: u16 = ptr::read_unaligned(kernel_buffer.add(HEADER_BOOT_FLAG) as *const u16);
+            let boot_flag: u16 =
+                ptr::read_unaligned(kernel_buffer.add(HEADER_BOOT_FLAG) as *const u16);
             if boot_flag != LINUX_BOOT_FLAG_MAGIC {
                 return false;
             }
@@ -22,15 +26,6 @@ impl BootProtocol for Linux {
         }
     }
 }
-use crate::efi::boot_services::EfiBootServices;
-use crate::efi::boot_services::constants::{
-    EFI_ALLOCATE_ANY_PAGES, EFI_ALLOCATE_MAX_ADDRESS, EFI_LOADER_CODE, EFI_LOADER_DATA,
-};
-use crate::efi::protocol::gop::mode::EfiGraphicsOutputProtocolMode;
-use crate::efi::system_table::EfiSystemTable;
-use crate::efi::EfiHandle;
-use crate::helpers::{check, halt};
-use crate::paging::constants::{PAGE_OFFSET_MASK, PAGE_SIZE};
 
 unsafe fn read_u8(base: *const u8, offset: usize) -> u8 {
     unsafe { *base.add(offset) }
@@ -52,14 +47,14 @@ unsafe fn write_u32(base: *mut u8, offset: usize, val: u32) {
     unsafe { ptr::write_unaligned(base.add(offset) as *mut u32, val); }
 }
 
-pub unsafe fn boot(
-    boot_services: *mut EfiBootServices,
-    image_handle: EfiHandle,
-    system_table: *mut EfiSystemTable,
+pub unsafe fn prepare<F: FirmwareInterface, A: Architecture>(
+    firmware: &mut F,
     kernel_buffer: *mut u8,
     kernel_size: usize,
-    gop_mode: *mut EfiGraphicsOutputProtocolMode,
-) -> ! {
+    boot_params: *mut u8,
+    cmdline_addr: u32,
+    fb_info: &FramebufferInfo,
+) -> u64 {
     unsafe {
         let mut setup_sects: u8 = read_u8(kernel_buffer, HEADER_SETUP_SECTS);
         if setup_sects == 0 {
@@ -71,37 +66,12 @@ pub unsafe fn boot(
         let init_size: u32 = read_u32(kernel_buffer, HEADER_INIT_SIZE);
         let handover_offset: u32 = read_u32(kernel_buffer, HEADER_HANDOVER_OFFSET);
 
-        let alloc_pages: usize = ((init_size as usize).max(pm_size) + PAGE_OFFSET_MASK as usize) / PAGE_SIZE as usize;
+        let alloc_pages: usize =
+            ((init_size as usize).max(pm_size) + A::PAGE_OFFSET_MASK as usize) / A::PAGE_SIZE as usize;
 
-        let mut pm_base: u64 = 0;
-        check(((*boot_services).allocate_pages)(
-            EFI_ALLOCATE_ANY_PAGES,
-            EFI_LOADER_CODE,
-            alloc_pages,
-            &mut pm_base,
-        ));
-        ptr::write_bytes(pm_base as *mut u8, 0, alloc_pages * PAGE_SIZE as usize);
+        let pm_base: u64 = firmware.allocate_pages(alloc_pages);
+        ptr::write_bytes(pm_base as *mut u8, 0, alloc_pages * A::PAGE_SIZE as usize);
         ptr::copy_nonoverlapping(kernel_buffer.add(pm_offset), pm_base as *mut u8, pm_size);
-
-        let mut boot_params_addr: u64 = 0xFFFFFFFF;
-        check(((*boot_services).allocate_pages)(
-            EFI_ALLOCATE_MAX_ADDRESS,
-            EFI_LOADER_DATA,
-            1,
-            &mut boot_params_addr,
-        ));
-        let boot_params: *mut u8 = boot_params_addr as *mut u8;
-        ptr::write_bytes(boot_params, 0, PAGE_SIZE as usize);
-
-        let mut cmdline_addr: u64 = 0xFFFFFFFF;
-        check(((*boot_services).allocate_pages)(
-            EFI_ALLOCATE_MAX_ADDRESS,
-            EFI_LOADER_DATA,
-            1,
-            &mut cmdline_addr,
-        ));
-        let cmdline_str: &[u8] = b"console=ttyS0,115200 nokaslr\0";
-        ptr::copy_nonoverlapping(cmdline_str.as_ptr(), cmdline_addr as *mut u8, cmdline_str.len());
 
         ptr::copy_nonoverlapping(
             kernel_buffer.add(HEADER_SETUP_SECTS),
@@ -110,46 +80,35 @@ pub unsafe fn boot(
         );
 
         write_u8(boot_params, HEADER_TYPE_OF_LOADER, 0xFF);
-        write_u8(boot_params, HEADER_LOADFLAGS, read_u8(boot_params, HEADER_LOADFLAGS) | LOADFLAGS_LOADED_HIGH | LOADFLAGS_CAN_USE_HEAP);
+        write_u8(
+            boot_params,
+            HEADER_LOADFLAGS,
+            read_u8(boot_params, HEADER_LOADFLAGS) | LOADFLAGS_LOADED_HIGH | LOADFLAGS_CAN_USE_HEAP,
+        );
         write_u16(boot_params, HEADER_HEAP_END_PTR, 0xFE00);
-        write_u32(boot_params, HEADER_CMD_LINE_PTR, cmdline_addr as u32);
+        write_u32(boot_params, HEADER_CMD_LINE_PTR, cmdline_addr);
         write_u32(boot_params, HEADER_RAMDISK_IMAGE, 0);
         write_u32(boot_params, HEADER_RAMDISK_SIZE, 0);
 
-        let info: *const _ = (*gop_mode).info;
-        let fb_base: u64 = (*gop_mode).frame_buffer_base;
-        let width: u32 = (*info).horizontal_resolution;
-        let height: u32 = (*info).vertical_resolution;
-        let pitch: u32 = (*info).pixels_per_scan_line * 4;
-        let fb_size: u32 = pitch * height;
-
-        let (red_size, red_pos, green_size, green_pos, blue_size, blue_pos): (u8, u8, u8, u8, u8, u8) =
-            match (*info).pixel_format {
-                0 => (8, 0, 8, 8, 8, 16),
-                1 => (8, 16, 8, 8, 8, 0),
-                _ => halt(),
-            };
+        let fb_size: u32 = fb_info.pitch * fb_info.height;
 
         write_u8(boot_params, SI_ORIG_VIDEO_IS_VGA, VIDEO_TYPE_EFI);
-        write_u16(boot_params, SI_LFB_WIDTH, width as u16);
-        write_u16(boot_params, SI_LFB_HEIGHT, height as u16);
+        write_u16(boot_params, SI_LFB_WIDTH, fb_info.width as u16);
+        write_u16(boot_params, SI_LFB_HEIGHT, fb_info.height as u16);
         write_u16(boot_params, SI_LFB_DEPTH, 32);
-        write_u32(boot_params, SI_LFB_BASE, fb_base as u32);
+        write_u32(boot_params, SI_LFB_BASE, fb_info.base as u32);
         write_u32(boot_params, SI_LFB_SIZE, fb_size);
-        write_u16(boot_params, SI_LFB_LINELENGTH, pitch as u16);
-        write_u8(boot_params, SI_RED_SIZE, red_size);
-        write_u8(boot_params, SI_RED_POS, red_pos);
-        write_u8(boot_params, SI_GREEN_SIZE, green_size);
-        write_u8(boot_params, SI_GREEN_POS, green_pos);
-        write_u8(boot_params, SI_BLUE_SIZE, blue_size);
-        write_u8(boot_params, SI_BLUE_POS, blue_pos);
-        write_u32(boot_params, SI_EXT_LFB_BASE, (fb_base >> 32) as u32);
+        write_u16(boot_params, SI_LFB_LINELENGTH, fb_info.pitch as u16);
+        write_u8(boot_params, SI_RED_SIZE, 8);
+        write_u8(boot_params, SI_RED_POS, fb_info.red_pos);
+        write_u8(boot_params, SI_GREEN_SIZE, 8);
+        write_u8(boot_params, SI_GREEN_POS, fb_info.green_pos);
+        write_u8(boot_params, SI_BLUE_SIZE, 8);
+        write_u8(boot_params, SI_BLUE_POS, fb_info.blue_pos);
+        write_u32(boot_params, SI_EXT_LFB_BASE, (fb_info.base >> 32) as u32);
 
-        ((*boot_services).free_pool)(kernel_buffer);
+        firmware.free_buffer(kernel_buffer);
 
-        let entry: u64 = pm_base + handover_offset as u64 + 512;
-        let handover: extern "sysv64" fn(EfiHandle, *mut EfiSystemTable, *mut u8) -> ! =
-            core::mem::transmute(entry);
-        handover(image_handle, system_table, boot_params);
+        pm_base + handover_offset as u64 + 512
     }
 }
